5c52ba489563184a6833187df00fd1388e6fabdd
Thinh2k04 <pham76121@gmail.com>
2024-10-18 00:03:40 +0700

    18/10/login/home

--- FE_AION/src/App.js
+++ FE_AION/src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import LoginPage from './login/page';
 import './App.css';
 import HomePage from './home/page';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    navigate('/home');
+  };
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="w-full max-w-md">
-        <HomePage />
+    <div className="w-full h-screen">
+      <div className="w-full h-full">
+        <Routes>
+          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+          <Route path="/home" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
+        </Routes>
       </div>
     </div>
   );
 }
-
-export default App;
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+export default AppWrapper;
--- FE_AION/src/home/page.jsx
+++ FE_AION/src/home/page.jsx
@@ -1,71 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HomePage = () => {
   const laptops = [
-    { id: 1, name: 'MacBook Air 15 inch M2 2023', price: '31.990.000₫', image: '/images/macbook-air-15-m2-2023.jpg', specs: ['Apple M2', '8GB', '256GB', '15.3 inch'] },
-    { id: 2, name: 'Asus TUF Gaming F15 FX506HF i5', price: '16.990.000₫', image: '/images/asus-tuf-gaming-f15-fx506hf-i5.jpg', specs: ['i5 11400H', '8GB', '512GB', '15.6"Full HD'] },
-    { id: 3, name: 'HP 15s fq5229TU i3 1215U', price: '10.990.000₫', image: '/images/hp-15s-fq5229tu-i3.jpg', specs: ['i3 1215U', '8GB', '256GB', '15.6"Full HD'] },
-    { id: 4, name: 'Lenovo Ideapad 3 15IAU7 i3', price: '9.990.000₫', image: '/images/lenovo-ideapad-3-15iau7-i3.jpg', specs: ['i3 1215U', '8GB', '256GB', '15.6"Full HD'] },
-    { id: 5, name: 'Acer Aspire 7 Gaming A715 76G 5132 i5', price: '17.990.000₫', image: '/images/acer-aspire-7-gaming-a715-76g-5132-i5.jpg', specs: ['i5 12450H', '8GB', '512GB', '15.6"Full HD'] },
-    { id: 6, name: 'Dell XPS 13 9310 i5', price: '28.990.000₫', image: '/images/dell-xps-13-9310-i5.jpg', specs: ['i5 1135G7', '8GB', '512GB SSD', '13.4"Full HD+'] },
-    { id: 7, name: 'MSI Modern 14 B11MOU i3', price: '11.990.000₫', image: '/images/msi-modern-14-b11mou-i3.jpg', specs: ['i3 1115G4', '8GB', '256GB SSD', '14"Full HD'] },
-    { id: 8, name: 'LG Gram 17 2021 i7', price: '39.990.000₫', image: '/images/lg-gram-17-2021-i7.jpg', specs: ['i7 1165G7', '16GB', '512GB SSD', '17"WQXGA'] },
-    { id: 9, name: 'Huawei MateBook D15 R5', price: '14.990.000₫', image: '/images/huawei-matebook-d15-r5.jpg', specs: ['Ryzen 5 3500U', '8GB', '256GB SSD', '15.6"Full HD'] },
-    { id: 10, name: 'Microsoft Surface Laptop 4 i5', price: '24.990.000₫', image: '/images/microsoft-surface-laptop-4-i5.jpg', specs: ['i5 1135G7', '8GB', '512GB SSD', '13.5"Touch'] },
+    { id: 1, name: 'MacBook Air 13 inch 2020', price: '23.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1', '8GB', '256GB', '13.3 inch'] },
+    { id: 2, name: 'MacBook Pro 13 inch 2022', price: '32.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M2', '8GB', '512GB', '13.3 inch'] },
+    { id: 3, name: 'MacBook Pro 14 inch 2021', price: '52.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Pro', '16GB', '512GB', '14.2 inch'] },
+    { id: 4, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 5, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 6, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 7, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 8, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 9, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 10, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 11, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 12, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 13, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 14, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 15, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 16, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 17, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 18, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 19, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 20, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 21, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 22, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 23, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 24, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 25, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 26, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 27, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 28, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 29, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 30, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 31, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 32, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 33, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 34, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 35, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 36, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 37, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 38, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
+    { id: 39, name: 'MacBook Pro 16 inch 2021', price: '62.990.000₫', image: 'https://media.studio7thailand.com/69968/MacBook_Pro_13-inch_Silver_1-square_medium.jpg', specs: ['M1 Max', '32GB', '1TB', '16.2 inch'] },
   ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 13;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = laptops.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(laptops.length / itemsPerPage);
+
+  const handleLoadMore = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <header className="bg-yellow-400 text-black py-2">
+      <header className="bg-green-900 text-white py-4">
         <div className="container mx-auto px-4">
           <div className="flex justify-between items-center">
-            <h1 className="text-2xl font-bold">Thế Giới Di Động</h1>
+            <nav className="flex space-x-4">
+              <a href="#" className="hover:underline">Thiết bị</a>
+              <a href="#" className="hover:underline">Mac</a>
+              <a href="#" className="hover:underline">Laptop</a>
+              <a href="#" className="hover:underline">Phụ kiện</a>
+              <a href="#" className="hover:underline">Tin tức</a>
+            </nav>
             <div className="flex items-center space-x-4">
-              <input type="text" placeholder="Bạn tìm gì..." className="px-3 py-1 rounded-full" />
-              <a href="#" className="hover:underline">Đăng nhập</a>
+              <input type="text" placeholder="Tìm kiếm" className="px-3 py-1 rounded-full" />
               <a href="#" className="hover:underline">Giỏ hàng</a>
             </div>
           </div>
         </div>
       </header>
-
       <main className="flex-grow container mx-auto py-4 px-4">
-        <div className="mb-4">
-          <img src="/images/laptop-banner.jpg" alt="Laptop Banner" className="w-full rounded-lg" />
+        <div className="mb-4 text-center">
+          <h2 className="text-2xl font-bold">Chuyển sang dùng Mac</h2>
+          <p className="text-lg">Thịnh đẹp trai vô địch vũ trụ</p>
+          <img src="https://cdnb.artstation.com/p/assets/images/images/016/802/459/large/shuja-shuaib-banner.jpg?1553535424" alt="MacBook Pro Banner" />
         </div>
-        <div className="mb-4">
-          <ul className="flex flex-wrap gap-2">
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">MacBook</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">ASUS</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">HP</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">Lenovo</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">Acer</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">Dell</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">MSI</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">Surface</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">LG</a></li>
-            <li><a href="#" className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm hover:bg-gray-300">GIGABYTE</a></li>
-          </ul>
+        <div className="text-center mb-4">
+          <h3 className="text-xl font-semibold">Mac</h3>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-          {laptops.map((laptop) => (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {currentItems.map((laptop, index) => (
             <div key={laptop.id} className="bg-white rounded-lg shadow-md overflow-hidden">
               <img src={laptop.image} alt={laptop.name} className="w-full h-48 object-cover" />
               <div className="p-3">
-                <h3 className="text-sm font-semibold mb-1">{laptop.name}</h3>
+                <h3 className="text-sm font-semibold mb-1">{indexOfFirstItem + index + 1}. {laptop.name}</h3>
                 <p className="text-red-600 font-bold text-lg mt-1">{laptop.price}</p>
                 <div className="mt-2">
                   {laptop.specs.map((spec, index) => (
                     <span key={index} className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-700 mr-1 mb-1">{spec}</span>
                   ))}
                 </div>
-                <div className="mt-2 text-xs text-blue-600">
-                  <span className="border border-blue-600 rounded px-1 mr-1">Trả góp 0%</span>
-                  <span className="border border-blue-600 rounded px-1">Giảm 1.000.000₫</span>
-                </div>
               </div>
             </div>
           ))}
+        </div>
+        <div className="flex justify-center mt-4 space-x-2">
+          <button onClick={handleLoadMore} disabled={currentPage === totalPages} className="px-4 py-2 bg-gray-300 rounded-md">
+            Xem thêm
+          </button>
+        </div>
+        <div className="text-center mt-4">
+          <p>Hiển thị {currentItems.length} trên {laptops.length} sản phẩm</p>
         </div>
       </main>
 
