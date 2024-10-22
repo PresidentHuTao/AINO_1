98a92081d13610a46980326ac28c96e7e2a4298b
thinh2k4 <pham76121@pham76121gmail.76121gmailcom>
2024-10-22 15:00:52 +0700

    remove

--- src/main/java/com/example/aino_1/repository/ManHinhInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.aino_1.repository;
-
-import com.example.aino_1.entity.ManHinh;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManHinhInterface extends JpaRepository<ManHinh, Integer> {
-}
--- src/main/java/com/example/aino_1/repository/SanPhamInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.aino_1.repository;
-
-import com.example.aino_1.entity.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SanPhamInterface extends JpaRepository<SanPham, Integer> {
-}
